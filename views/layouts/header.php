e9445a45500421dc6940168708ccde6a48e7229e
Pedro Luis Rodriguez Amador <rodriguezamador90@gmail.com>
2019-03-06 09:22:30 +0100

    Se corrigue nombre del Home cuando se minimiza el sidebar

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -14,7 +14,7 @@ use yii\helpers\Html;
 
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">Loan</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini">SGV</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
